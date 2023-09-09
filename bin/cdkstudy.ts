e6aa3573768b9ec62f177ab181175509d615f765
kfjt <k.fujita@jcom.home.ne.jp>
2023-09-09 01:43:39 +0000

    cdk-nag

--- bin/cdkstudy.ts
+++ bin/cdkstudy.ts
@@ -1,21 +1,15 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkstudyStack } from '../lib/cdkstudy-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { AwsSolutionsChecks } from "cdk-nag";
+import { CdkstudyStack } from "../lib/cdkstudy-stack";
 
 const app = new cdk.App();
-new CdkstudyStack(app, 'CdkstudyStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
+new CdkstudyStack(app, "CdkstudyStack", {
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
+cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
