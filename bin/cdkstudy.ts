18d1d2303d1b9a8e996cb717f7f58d849a94eb9d
kfjt <k.fujita@jcom.home.ne.jp>
2023-09-10 06:03:54 +0000

    multi env

--- bin/cdkstudy.ts
+++ bin/cdkstudy.ts
@@ -3,13 +3,22 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { AwsSolutionsChecks } from "cdk-nag";
 import { ReferredStack } from "../lib/referred-stack";
+import { AppStack } from "../lib/app-stack";
 
 const app = new cdk.App();
-new ReferredStack(app, "ReferredStack", {
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+
+const envValsDev = app.node.tryGetContext("dev");
+const envDev = {
+  account: envValsDev["env"]["account"],
+  region: envValsDev["env"]["region"],
+};
+
+const envValsPrd = app.node.tryGetContext("prd");
+const envPrd = {
+  account: envValsPrd["env"]["account"],
+  region: envValsPrd["env"]["region"],
+};
+
+new ReferredStack(app, "ReferredStack", { env: envDev });
+new AppStack(app, "AppStack", { env: envPrd });
 cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
